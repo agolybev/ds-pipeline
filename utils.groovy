3b1789a3fc3d34f32b189a744c72519ca26d643f
Alexey Golubev <Alexey.Golubev@onlyoffice.com>
2019-12-04 19:27:02 +0300

    Remove paid features for opensource build on Win

--- utils.groovy
+++ utils.groovy
@@ -105,7 +105,6 @@ def windowsBuild(String branch = 'master', String platform = 'native', Boolean c
             --update false\
             --branch ${branch}\
             --clean ${clean.toString()}\
-            --sdkjs-addon sdkjs-comparison\
             --qt-dir \"C:\\Qt\\Qt5.9.8\\5.9.8\"\
             --qt-dir-xp \"C:\\Qt\\Qt5.6.3\\5.6.3\""
 
