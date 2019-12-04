d232fb0060c53f79c73bc9da711f9e6ff315e4f2
Alexey Golubev <Alexey.Golubev@onlyoffice.com>
2019-12-04 16:52:24 +0300

    Fix mistype

--- utils.groovy
+++ utils.groovy
@@ -67,7 +67,7 @@ def linuxBuild(String branch = 'master', String platform = 'native', Boolean cle
 {
     checkoutRepos(branch)
 
-    String confParams = "
+    String confParams = "\
       --module \"server\"
       --platform ${platform}\
       --update false\
@@ -108,7 +108,7 @@ def windowsBuild(String branch = 'master', String platform = 'native', Boolean c
             --sdkjs-addon sdkjs-comparison\
             --qt-dir \"C:\\Qt\\Qt5.9.8\\5.9.8\"\
             --qt-dir-xp \"C:\\Qt\\Qt5.6.3\\5.6.3\""
-            
+
     if (noneFree) {
       confParams = confParams.concant("--sdkjs-addon sdkjs-comparison")
     }
